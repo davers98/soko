28502c9dc9bf7eb96a7d269e22c054ad895f11c2
davers98 <davidrobert785@gmail.com>
2022-05-20 15:20:30 +0300

    auth screens and functionality

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //
+    public function register()
+    {
+
+        return view('auth.register');
+    }
+
+    public function storeRegister(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'password_confirmation' => 'required',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect('login');
+    }
+
+    public function login()
+    {
+
+        return view('auth.login');
+    }
+
+    public function storeLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('home');
+        }
+
+        return redirect('login')->with('error', 'Oppes! You have entered invalid credentials');
+    }
+
+    public function logout() {
+        Auth::logout();
+
+        return redirect('login');
+    }
+
+    public function home()
+    {
+
+        return view('home');
+    }
+}
+
